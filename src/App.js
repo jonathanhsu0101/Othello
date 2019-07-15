d3b3e72d962ff57cc6e21b90582575d54cffa3c4
jonathanhsu0101 <jonathan.hsu0101@gmail.com>
2019-07-14 21:08:06 -0600

    finished human move

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ class App extends Component {
   reversi = [
     ['.', '.', '.', '.', '.', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', 'O', 'X', '.', '.', '.'],
-    ['.', '.', '.', 'X', 'O', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', 'O', '.', '.'],
+    ['.', '.', 'O', 'O', 'X', '.', '.', '.'],
+    ['.', '.', '.', 'X', 'O', 'O', '.', '.'],
+    ['.', '.', 'O', '.', '.', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.', '.', '.']
   ];
@@ -28,9 +28,9 @@ class App extends Component {
       return this.reversi[i][j] === '.';
     };
 
+    // searching up
     const cond1 = (i, j) => {
       let legit = true;
-      console.log('i = ' + i);
       if (i > 1 && this.reversi[i - 1][j] === 'O') {
         for (let a = i - 2; a >= 0; a--) {
           if (legit && this.reversi[a][j] === 'X') {
@@ -47,8 +47,174 @@ class App extends Component {
       return false;
     };
 
-    return cond0(i, j) && cond1(i, j);
-    // return cond0(i, j) && (cond1(i, j) || cond2(i,j) || cond3(i,j) || cond4(i,j) || cond5(i,j) || cond6(i,j) || cond7(i,j) || cond8(i,j);
+    // searching down
+    const cond2 = (i, j) => {
+      let legit = true;
+      if (i < 6 && this.reversi[i + 1][j] === 'O') {
+        for (let a = i + 2; a <= 7; a++) {
+          if (legit && this.reversi[a][j] === 'X') {
+            for (let b = a - 1; b > i; b--) {
+              this.reversi[b][j] = 'X';
+            }
+            return true;
+          }
+          if (this.reversi[a][j] !== 'O') {
+            legit = false;
+          }
+        }
+      }
+      return false;
+    };
+
+    // searching right
+    const cond3 = (i, j) => {
+      let legit = true;
+      if (j < 6 && this.reversi[i][j + 1] === 'O') {
+        for (let a = j + 2; a <= 7; a++) {
+          if (legit && this.reversi[i][a] === 'X') {
+            for (let b = a - 1; b > j; b--) {
+              this.reversi[i][b] = 'X';
+            }
+            return true;
+          }
+          if (this.reversi[i][a] !== 'O') {
+            legit = false;
+          }
+        }
+      }
+      return false;
+    };
+
+    // searching left
+    const cond4 = (i, j) => {
+      let legit = true;
+      if (j > 1 && this.reversi[i][j - 1] === 'O') {
+        for (let a = j - 2; a >= 0; a--) {
+          if (legit && this.reversi[i][a] === 'X') {
+            for (let b = a + 1; b < j; b++) {
+              this.reversi[i][b] = 'X';
+            }
+            return true;
+          }
+          if (this.reversi[i][a] !== 'O') {
+            legit = false;
+          }
+        }
+      }
+      return false;
+    };
+
+    // searching down-right
+    const cond5 = (i, j) => {
+      let legit = true;
+      if (i < 6 && j < 6 && this.reversi[i + 1][j + 1] === 'O') {
+        let a = i + 1;
+        let b = j + 1;
+        while (a < 7 && b < 7) {
+          a++;
+          b++;
+          if (legit && this.reversi[a][b] === 'X') {
+            while (a > i) {
+              a--;
+              b--;
+              this.reversi[a][b] = 'X';
+            }
+            return true;
+          }
+          if (this.reversi[a][b] !== 'O') {
+            legit = false;
+          }
+        }
+      }
+      return false;
+    };
+
+    // searching up-left
+    const cond6 = (i, j) => {
+      let legit = true;
+      if (i > 1 && j > 1 && this.reversi[i - 1][j - 1] === 'O') {
+        let a = i - 1;
+        let b = j - 1;
+        while (a > 0 && b > 0) {
+          a--;
+          b--;
+          if (legit && this.reversi[a][b] === 'X') {
+            while (a < i) {
+              a++;
+              b++;
+              this.reversi[a][b] = 'X';
+            }
+            return true;
+          }
+          if (this.reversi[a][b] !== 'O') {
+            legit = false;
+          }
+        }
+      }
+      return false;
+    };
+
+    // searching up-right
+    const cond7 = (i, j) => {
+      let legit = true;
+      if (i > 1 && j < 6 && this.reversi[i - 1][j + 1] === 'O') {
+        let a = i - 1;
+        let b = j + 1;
+        while (a > 0 && b < 7) {
+          a--;
+          b++;
+          if (legit && this.reversi[a][b] === 'X') {
+            while (a < i) {
+              a++;
+              b--;
+              this.reversi[a][b] = 'X';
+            }
+            return true;
+          }
+          if (this.reversi[a][b] !== 'O') {
+            legit = false;
+          }
+        }
+      }
+      return false;
+    };
+
+    // searching down-left
+    const cond8 = (i, j) => {
+      let legit = true;
+      if (i < 6 && j > 1 && this.reversi[i + 1][j - 1] === 'O') {
+        let a = i + 1;
+        let b = j - 1;
+        while (a < 7 && b > 0) {
+          a++;
+          b--;
+          if (legit && this.reversi[a][b] === 'X') {
+            while (a > i) {
+              a--;
+              b++;
+              this.reversi[a][b] = 'X';
+            }
+            return true;
+          }
+          if (this.reversi[a][b] !== 'O') {
+            legit = false;
+          }
+        }
+      }
+      return false;
+    };
+
+    return (
+      cond0(i, j) &&
+      (cond1(i, j) ||
+        cond2(i, j) ||
+        cond3(i, j) ||
+        cond4(i, j) ||
+        cond5(i, j) ||
+        cond6(i, j) ||
+        cond7(i, j) ||
+        cond8(i, j))
+    );
   };
 
   nextMove = (i, j) => {
@@ -87,7 +253,7 @@ class App extends Component {
         <header style={{ color: 'blue', fontSize: '100px' }}>
           R e v e r s i
         </header>
-        <div>{this.createTable()}</div>;
+        <div>{this.createTable()}</div>
       </div>
     );
   }
